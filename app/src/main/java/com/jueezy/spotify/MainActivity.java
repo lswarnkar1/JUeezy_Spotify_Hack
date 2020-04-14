e4085f237ae532220eaf98d2c52ee660ec87cb81
Hey-Deepak <choudharydeepak990@gmail.com>
2020-04-14 22:55:46 +0530

    Update MainActivity.java

--- app/src/main/java/com/jueezy/spotify/MainActivity.java
+++ app/src/main/java/com/jueezy/spotify/MainActivity.java
@@ -6,8 +6,13 @@ import android.content.ComponentName;
 import android.content.Intent;
 import android.os.Bundle;
 import android.provider.Settings;
+import android.text.Layout;
 import android.text.TextUtils;
+import android.util.Log;
+import android.view.View;
 import android.widget.Button;
+import android.widget.CompoundButton;
+import android.widget.LinearLayout;
 import android.widget.Switch;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -17,27 +22,70 @@ import static android.widget.Toast.LENGTH_LONG;
 
 public class MainActivity extends AppCompatActivity {
 
+    TextView rootDescription;
+    LinearLayout leftLayout, rightLayout;
+    Switch sw1, sw2;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        TextView rootDescription;
-        Switch sw1, sw2;
 
         rootDescription = (TextView) findViewById(R.id.root_description);
+        leftLayout = (LinearLayout) findViewById(R.id.left_layout);
+        rightLayout = (LinearLayout) findViewById(R.id.right_layout);
+        sw1 = (Switch) findViewById(R.id.muter);
+        sw2 = (Switch) findViewById(R.id.killer);
+
 
         if (RootUtil.isDeviceRooted()) {
-            Toast.makeText(this, "Your Decice is Rooted",LENGTH_LONG ).show();
+            Toast.makeText(this, "Your Decice is Rooted", LENGTH_LONG).show();
+            rootDescription.setText("Your Devive is Rooted");
         } else {
             Toast.makeText(this, "Your Decice is Non-Rooted", LENGTH_LONG).show();
             rootDescription.setText("Your Devive is Non-Rooted");
+            /*rightLayout.setVisibility(View.GONE);*/
         }
-        /*if(!isNotificationServiceEnabled())
-            startActivity(new Intent(ACTION_NOTIFICATION_LISTENER_SETTINGS));*/
+
+        sw1.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked) {
+            if (!isNotificationServiceEnabled()) {
+                    startActivity(new Intent(ACTION_NOTIFICATION_LISTENER_SETTINGS));
+              }
+                    } else {
+
+                }
+            }
+        });
+
+        sw2.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked) {
+                    if (!isNotificationServiceEnabled()) {
+                        startActivity(new Intent(ACTION_NOTIFICATION_LISTENER_SETTINGS));
+                    }
+                } else {
+
+                }
+            }
+        });
     }
 
-    private boolean isNotificationServiceEnabled(){
+    @Override
+    protected void onPostResume() {
+        super.onPostResume();
+        if (!isNotificationServiceEnabled()){
+            sw1.setChecked(false);
+            sw2.setChecked(false);
+        }else {
+            sw1.setChecked(true);
+            sw2.setChecked(true);
+        }
+    }
+
+    private boolean isNotificationServiceEnabled() {
         String pkgName = getPackageName();
         final String flat = Settings.Secure.getString(getContentResolver(),
                 "enabled_notification_listeners");
