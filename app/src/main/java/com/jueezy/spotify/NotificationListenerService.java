5df6c4baf9e882298f37e905aed8712f2451f091
Hey-Deepak <choudharydeepak990@gmail.com>
2020-04-17 19:25:31 +0530

    NLC

--- app/src/main/java/com/jueezy/spotify/NotificationListenerService.java
+++ app/src/main/java/com/jueezy/spotify/NotificationListenerService.java
@@ -46,8 +46,8 @@ public class NotificationListenerService extends android.service.notification.No
         }
 
         if (isMute) {
-            /*Log.d("DC", "Mute On");
-            Toast.makeText(this, "Mute Option", Toast.LENGTH_SHORT).show();*/
+            Log.d("DC", "Mute On");
+            Toast.makeText(this, "Mute Option", Toast.LENGTH_SHORT).show();
             if (pack.equals("com.spotify.music")) {
                 Notification.Action[] actions = sbn.getNotification().actions;
                 if (actions.length == 3)
